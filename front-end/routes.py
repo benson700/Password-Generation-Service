e76d039f3162629f4b55c1fc241478229fb7e118
benson700 <idbobs@gmail.com>
2022-04-07 09:07:17 +0000

    new commit

--- front-end/routes.py
+++ front-end/routes.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder='template', static_folder='static')
 @app.route('/', methods=['GET'])
 def password_length():
     length = request.args.get('password-length')
-    print(length)
+
 
     # do something
     # value captured from the frontend is stored in variable - length
